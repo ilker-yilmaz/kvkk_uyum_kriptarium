a5279ce7b10a5d8e890cf86d07112cf39d8c1bc4
ilker yilmaz <ilkery549@gmail.com>
2022-08-04 20:43:06 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { SignUpAmatorComponent } from './components/sign-up-amator/sign-up-amator.component';
+import { SignInAmatorComponent } from './components/sign-in-amator/sign-in-amator.component';
+import { VeriGirisComponent } from './components/veri-giris/veri-giris.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInAmatorComponent },
+  { path: 'sign-up', component: SignUpAmatorComponent },
+  { path: 'veri-giris', component: VeriGirisComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: '',   redirectTo: '/veri-giris-component', pathMatch: 'full' }, // redirect to `first-component`
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
