c00b7d887b05a7999e9b44d2d74100a0f7fd7990
ilker yilmaz <ilkery549@gmail.com>
2022-08-24 19:06:02 +0300

    added models and changed ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { VeriGirisComponent } from './components/veri-giris/veri-giris.component
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KvkkEnvanterComponent } from './components/kvkk-envanter/kvkk-envanter.component';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInAmatorComponent },
   { path: 'sign-up', component: SignUpAmatorComponent },
   { path: 'veri-giris', component: VeriGirisComponent },
   { path: 'user-profile', component: UserProfileComponent },
+  { path: 'kvkk-envanter', component: KvkkEnvanterComponent },
   { path: '',   redirectTo: '/veri-giris', pathMatch: 'full' }, // redirect to `first-component`
 
 ];
